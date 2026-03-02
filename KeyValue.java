9c46eb0662194dfbb2f628c4674d894f7b60d04e
hond64 <wentzelmiguel@gmail.com>
2026-03-02 13:45:22 +0000

    Create keyvalue  class and create static variables

--- KeyValue.java
+++ KeyValue.java
@@ -1,1 +1,12 @@
+//Miguel Wentzel
+//4478677
+//Practical 4
 
+public class KeyValues{
+    static String key;
+    static String value;
+    public KeyValue(String key,String value){
+        this.key= key;
+        this.value = value;
+    }
+}
